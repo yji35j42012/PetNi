27a4e29315f2016104dfef0602307f2c3cf9e49a
yji35j4212 <yji35j4212@gmail.com>
2022-03-11 11:22:50 +0800

    檔案同步

--- page/home.vue
+++ page/home.vue
@@ -1,7 +1,7 @@
 <style scoped></style>
 
 <template>
-    <div class="container">
+    <div class="container detail">
         <div class="func">
             <div class="func_box">
                 <h2>我想尋找</h2>
@@ -92,22 +92,115 @@
             </div>
             <button class="btn">套用</button>
         </div>
+        <div class="headFunc">
+            <button class="backCard">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 46 46">
+                    <path :d="icon_all.back" />
+                </svg>
+            </button>
+            <span class="name">雙色狗</span>
+            <button class="searchBtn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 38 38">
+                    <path :d="icon_all.filter" />
+                </svg>
+            </button>
+        </div>
         <div class="info">
             <div class="slip">
-                <div class="slip_item"></div>
+                <div class="slip_item">
+                    <button class="slip_item_detail">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 51 51"
+                        >
+                            <path :d="icon_all.detail" />
+                        </svg>
+                    </button>
+                    <div class="slip_item_box">
+                        <button class="slip_item_unlike">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 25 25"
+                            >
+                                <path :d="icon_all.unlike" />
+                            </svg>
+                        </button>
+                        <div class="slip_item_info">
+                            <p class="name female">
+                                157763
+                                <i class="gender">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path :d="icon_all.female" />
+                                    </svg>
+                                </i>
+                            </p>
+                            <p class="address">臺南市南區</p>
+                        </div>
+                        <button class="slip_item_like">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 32 32"
+                            >
+                                <path :d="icon_all.like" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
             </div>
             <ul class="recommend">
                 <li>
-                    <div class="pic"></div>
-                    <div class="recommend_info"></div>
+                    <div class="recommend_pic"></div>
+                    <div class="recommend_info">
+                        <p class="name female">
+                            157763
+                            <i class="gender">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path :d="icon_all.female" />
+                                </svg>
+                            </i>
+                        </p>
+                        <p class="address">臺南市南區</p>
+                    </div>
                 </li>
                 <li>
-                    <div class="pic"></div>
-                    <div class="recommend_info"></div>
+                    <div class="recommend_pic"></div>
+                    <div class="recommend_info">
+                        <p class="name">
+                            157763
+                            <i class="gender female">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path :d="icon_all.female" />
+                                </svg>
+                            </i>
+                        </p>
+                        <p class="address">臺南市南區</p>
+                    </div>
                 </li>
                 <li>
-                    <div class="pic"></div>
-                    <div class="recommend_info"></div>
+                    <div class="recommend_pic"></div>
+                    <div class="recommend_info">
+                        <p class="name">
+                            157763
+                            <i class="gender male">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path :d="icon_all.male" />
+                                </svg>
+                            </i>
+                        </p>
+                        <p class="address">臺南市南區</p>
+                    </div>
                 </li>
             </ul>
         </div>
