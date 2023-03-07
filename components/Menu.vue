5b30b9165775373bf971387ca03a2d89617ed9e0
yji35j4212 <yji35j4212@gmail.com>
2023-03-07 16:40:43 +0800

    檔案同步

--- components/Menu.vue
+++ components/Menu.vue
@@ -3,23 +3,35 @@
 <template>
 	<div class="menu">
 		<div class="logo">
-			<img class="logo_img" src="./images/logo.png" alt />
+			<router-link to="/home">
+				<img class="logo_img" src="./images/logo.png" alt />
+			</router-link>
 		</div>
 		<ul class="menu_nav">
-			<li :class="['menu_nav_item', menuItem==1?'on':'']" @click="menuHandler(1)" data-txt="配對">
-				<i v-html="icon_all.pair"></i>
+			<li :class="['menu_nav_item', menuItem==1?'on':'']" @click="menuHandler(1)">
+				<router-link to="/home" data-txt="配對">
+					<i v-html="icon_all.pair"></i>
+				</router-link>
 			</li>
-			<li :class="['menu_nav_item', menuItem==2?'on':'']" @click="menuHandler(2)" data-txt="收藏">
-				<i v-html="icon_all.favorites"></i>
+			<li :class="['menu_nav_item', menuItem==2?'on':'']" @click="menuHandler(2)">
+				<router-link to="/" data-txt="收藏">
+					<i v-html="icon_all.favorites"></i>
+				</router-link>
 			</li>
-			<li :class="['menu_nav_item', menuItem==3?'on':'']" @click="menuHandler(3)" data-txt="主題館">
-				<i v-html="icon_all.theme"></i>
+			<li :class="['menu_nav_item', menuItem==3?'on':'']" @click="menuHandler(3)">
+				<router-link to="/" data-txt="主題館">
+					<i v-html="icon_all.theme"></i>
+				</router-link>
 			</li>
-			<li :class="['menu_nav_item', menuItem==4?'on':'']" @click="menuHandler(4)" data-txt="送養">
-				<i v-html="icon_all.collar"></i>
+			<li :class="['menu_nav_item', menuItem==4?'on':'']" @click="menuHandler(4)">
+				<router-link to="/" data-txt="送養">
+					<i v-html="icon_all.collar"></i>
+				</router-link>
 			</li>
-			<li :class="['menu_nav_item', menuItem==5?'on':'']" @click="menuHandler(5)" data-txt="急診">
-				<i v-html="icon_all.hospital"></i>
+			<li :class="['menu_nav_item', menuItem==5?'on':'']" @click="menuHandler(5)">
+				<router-link to="/emergency" data-txt="急診">
+					<i v-html="icon_all.hospital"></i>
+				</router-link>
 			</li>
 		</ul>
 	</div>
--- page/emergency.vue
+++ page/emergency.vue
@@ -2,41 +2,39 @@
 
 <template>
 	<div class="container">
-		<div style="display:none">{{ showLists }}</div>
-		<div class="emergency_head">
+		<div class="emergency_nav">
 			<div class="name showPAD" data-txt="急診">24H</div>
 			<ul class="tab">
-				<li @click="emergency_area('N')" :class="area == 'N' ? 'on' : ''">北部</li>
-				<li @click="emergency_area('W')" :class="area == 'W' ? 'on' : ''">中部</li>
-				<li @click="emergency_area('S')" :class="area == 'S' ? 'on' : ''">南部</li>
-				<li @click="emergency_area('E')" :class="area == 'E' ? 'on' : ''">東部</li>
+				<li class="tab_item" @click="emergency_area('N')" :class="area == 'N' ? 'on' : ''">北部</li>
+				<li class="tab_item" @click="emergency_area('W')" :class="area == 'W' ? 'on' : ''">中部</li>
+				<li class="tab_item" @click="emergency_area('S')" :class="area == 'S' ? 'on' : ''">南部</li>
+				<li class="tab_item" @click="emergency_area('E')" :class="area == 'E' ? 'on' : ''">東部</li>
 			</ul>
 			<span>*資訊僅供參考，建議先電話聯絡再前往。</span>
 		</div>
-
-		<ul class="emergency_group" v-for="(outItem, outIndex) in showLists.city_map" :key="outIndex">
-			<li class="emergency_group_title">
-				<i v-html="icon_all.address"></i>
-				{{ showLists.city_map[outIndex] }}
-			</li>
-			<li
-				class="emergency_group_card"
-				v-for="(insideItem, insideIndex) in showLists.list_obj[
+		<div class="scroll_y">
+			<ul class="emergency_group" v-for="(outItem, outIndex) in showLists.city_map" :key="outIndex">
+				<li class="emergency_group_title">
+					<i v-html="icon_all.address"></i>
+					{{ showLists.city_map[outIndex] }}
+				</li>
+				<li
+					class="emergency_group_card"
+					v-for="(insideItem, insideIndex) in showLists.list_obj[
 					showLists.city_map[outIndex]
 				]"
-				:key="insideIndex"
-			>
-				<div class="detail">
-					<p class="title">{{ insideItem.name }}</p>
+					:key="insideIndex"
+				>
+					<p class="title">
+						{{ insideItem.name }}
+						<span :class="insideItem.epidemic ? '' : 'hide'">*疫情期間有開</span>
+					</p>
 					<p>{{ insideItem.phone }}</p>
-					<p>{{ insideItem.address }}</p>
-				</div>
-				<div class="state">
-					<span :class="insideItem.epidemic ? '' : 'hide'">*疫情期間有開</span>
-					<a href v-html="icon_all.phone"></a>
-				</div>
-			</li>
-		</ul>
+					<p class="address">{{ insideItem.address }}</p>
+					<a class="phone" href v-html="icon_all.phone"></a>
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
